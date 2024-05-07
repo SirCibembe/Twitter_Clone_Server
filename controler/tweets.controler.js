99402aad63d9beb8a107b1d4ffaf1d35b29f3c3a
SirCibembe <144097928+SirCibembe@users.noreply.github.com>
2024-05-07 11:17:19 +0200

    Update tweets.controler.js

--- controler/tweets.controler.js
+++ controler/tweets.controler.js
@@ -1,14 +1,1 @@
-import data from "../data.js";
-
-let tweetsTable = [...data.tweets.reverse()];
-
-export function tweetsControler(req, res) {
-  res.send(tweetsTable);
-}
-
-export function postTweetsControler(req, res) {
-  let newTweet = [req.body, ...tweetsTable];
-  tweetsTable = newTweet;
-  res.send(req.body);
-}
 
